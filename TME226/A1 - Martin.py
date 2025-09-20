697f5a91e224cccc77a265368dcee9faedee81ed
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-09-20 17:09:15 +0200

    .

--- TME226/A1 - Martin.py
+++ TME226/A1 - Martin.py
@@ -39,7 +39,7 @@ plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-plt.rc('figure', figsize=(8.5, 5))
+plt.rc('figure', figsize=(8, 5))
 
 
 def plotta_subplots(
