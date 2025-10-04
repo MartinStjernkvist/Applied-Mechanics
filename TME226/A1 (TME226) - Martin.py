4c4181b2e8c634fe7b139e72e7ec918d432567b4
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-10-04 17:13:39 +0200

    .

--- TME226/A1 (TME226) - Martin.py
+++ TME226/A1 (TME226) - Martin.py
@@ -43,7 +43,7 @@ plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-plt.rc('figure', figsize=(5, 4))
+plt.rc('figure', figsize=(8,4))
 
 dpi = 300
 
