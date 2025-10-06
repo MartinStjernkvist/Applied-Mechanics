5709803e22815197b571d62015ea51c15b9d8787
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-10-06 16:22:09 +0200

    .

---
+++ TME235/funcs_n_imports.py
@@ -0,0 +1,52 @@
+#%%
+# %matplotlib inline
+from scipy.optimize import fsolve
+# from scipy.differentiate import hessian
+import numpy as np
+from numpy import einsum
+import matplotlib.pyplot as plt
+import sympy as sp
+from IPython.display import display, Math
+from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.mplot3d import Axes3D
+from numpy.random import rand
+from IPython.display import HTML
+from matplotlib import animation
+
+import scipy.io as sio
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+
+
+import numpy as np
+from sympy import *
+import matplotlib.pyplot as plt
+from matplotlib import rcParams # for changing default values
+import matplotlib.ticker as ticker
+
+##################################################
+# Functions
+##################################################
+
+def new_prob(num):
+    print_string = '\n----------------------\n' + 'Assignment E' + str(num) + '\n----------------------\n'
+    return print(print_string)
+
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 14
+
+# Set the global font sizes
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.rc('figure', figsize=(6,3))
+
+dpi = 300
+
+#
