56865bcde8e940cd343eb590b568b9d21772f020
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-10-08 22:32:22 +0200

    .

--- TME235/read_abaqus.py
+++ TME235/read_abaqus.py
@@ -3,6 +3,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 14
+
+# Set the global font sizes
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.rc('figure', figsize=(6,3))
+
+dpi = 500
+
 datasets = {}
 current_data = []
 current_name = None
