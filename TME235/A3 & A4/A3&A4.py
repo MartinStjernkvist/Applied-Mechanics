4bcd0e1f6242d7569c45247f7c8a03fafbdafb6b
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-10-11 18:37:49 +0200

    .

---
+++ TME235/A3 & A4/A3&A4.py
@@ -0,0 +1,102 @@
+#%%
+# %matplotlib widget
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import *
+import math
+import pandas as pd
+
+from IPython.display import display, Math
+from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.mplot3d import Axes3D
+from numpy.random import rand
+from IPython.display import HTML
+from matplotlib import animation
+import scipy.io as sio
+from scipy.optimize import fsolve
+from matplotlib import rcParams # for changing default values
+import matplotlib.ticker as ticker
+
+import calfem.core as cfc
+import calfem.vis_mpl as cfv
+import calfem.mesh as cfm
+import calfem.utils as cfu
+
+from scipy.sparse import coo_matrix, csr_matrix
+import matplotlib.cm as cm
+
+from pathlib import Path
+
+def new_prob(string):
+    print_string = '\n--------------------------------------------\n' + 'Assignment ' + str(string) + '\n--------------------------------------------\n'
+    return print(print_string)
+
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 14
+dpi = 500
+
+# Set the global font sizes
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.rc('figure', figsize=(8,4))
+
+script_dir = Path(__file__).parent
+
+def sfig(fig_name):
+    fig_output_file = script_dir / "figures" / fig_name
+    fig_output_file.parent.mkdir(parents=True, exist_ok=True)
+    plt.savefig(fig_output_file, dpi=dpi, bbox_inches='tight')
+    
+def fig(fig_name):
+    '''
+    standard matplotlib commands
+    '''
+    plt.legend()
+    plt.grid(True, alpha = 0.3)
+    sfig(fig_name)
+    plt.show()
+
+#%%
+####################################################################################################
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
+
+
+# GIVEN INFORMATION
+
+
+
+####################################################################################################
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
+
+#%%
+####################################################################################################
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
+
+
+# Assignment 1 - ...
+
+
+
+####################################################################################################
+####################################################################################################
+####################################################################################################
+####################################################################################################
+new_prob('1 - ...')
+
+#%%
