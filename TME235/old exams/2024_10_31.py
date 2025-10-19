3813a164b3ab611312aa0c37aa3230dc2ad7ecf5
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-10-20 01:49:02 +0200

    .

--- TME235/old exams/2024_10_31.py
+++ TME235/old exams/2024_10_31.py
@@ -1,16 +1,21 @@
 #%%
 import numpy as np
 import sympy as sp
+import matplotlib.pyplot as plt
 
 #%%
 
+###########################################################################
 # Problem 2
+###########################################################################
 
-q0, r, a, b_minus_a, M, D, E, nu = sp.symbols('q0 r a b_minus_a M D E nu', real='True')
+q0, r, a, b_minus_a, M, E, nu, h = sp.symbols('q0 r a b_minus_a M E nu h', real='True')
+
+D = E * h**3 / (12 * (1 - nu**2))
 
 w = sp.Function('w')(r)
 
-diffeq = sp.Eq(1 / r * sp.diff(r * sp.diff(1 / r * sp.diff(r * sp.diff(w, r), r), r), r), q0 * (r - a)/ (b_minus_a))
+diffeq = sp.Eq(1 / r * sp.diff(r * sp.diff(1 / r * sp.diff(r * sp.diff(w, r), r), r), r), q0 * (r - a)/ (b_minus_a) / D)
 
 w_ = sp.dsolve(diffeq, w).rhs
 
@@ -32,10 +37,22 @@ w_sol = w_.subs(ic).simplify()
 
 print(w_sol)
 
-w_func = sp.lambdify((q0, r, a, b_minus_a, M, D, E, nu), w_sol, 'numpy')
+w_func = sp.lambdify((q0, r, a, b_minus_a, M, E, nu, h), w_sol, 'numpy')
 
-value = w_func(q0=1, r=1, a=1, b_minus_a=1, M=1, D=1, E=1, nu=1)
+r_vals = np.linspace(0.09, 0.3, 300)
+vals = w_func(q0=1e6, r=r_vals, a=0.1, b_minus_a=0.2, M=100, E=80e9, nu=0.3, h=0.01)
 # value = w_func(1,1,1,1,1,1,1,1)
-print(value)
+print(np.argmax(vals))
+print(np.argmin(vals))
+
+plt.figure()
+plt.plot(r_vals, vals)
+plt.show()
+
+#%%
+
+###########################################################################
+# Problem 3
+###########################################################################
 
 #%%
