347ef889fd967c0a697e746d7ebb798bf2192534
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-10-26 23:07:45 +0100

    .

--- TME235/old exams/2023_08_24.py
+++ TME235/old exams/2023_08_24.py
@@ -105,3 +105,75 @@ print(eigvals)
 
 print('\neigenvectors:')
 print(eigvecs)
+
+
+#%%
+# 3
+
+x, t, rho, gamma, omega, A, E, I, L, C1, C2, C3, C4 = sp.symbols('x t rho gamma omega A E I L C1 C2 C3 C4', real=True)
+
+W = C1 * sp.sin(gamma * x / L) + C2 * sp.cos(gamma * x / L) + C3 * sp.sinh(gamma * x / L) + C4 * sp.cosh(gamma * x / L)
+
+W_4 = W.diff(x, 4)
+
+w = W * sp.sin(omega * t)
+
+M = - E * I * w.diff(x, 2)
+
+w_sub_0 = w.subs(x, 0)
+print('\n0 deflection at x=0:')
+print(w_sub_0)
+
+M_sub_0 = M.subs(x, 0)
+print('\n0 moment at x=0:')
+print(M_sub_0)
+
+w_sub_L = w.subs([(x, L), (C2, 0), (C4, 0)])
+print('\n0 deflection at x=L:')
+print(w_sub_L)
+
+M_sub_L = M.subs([(x, L), (C2, 0), (C4, 0)])
+print('\n0 moment at x=L:')
+print(M_sub_L)
+
+
+
+#%%
+# 3
+
+g = 9.81
+rho = 7800
+r = 1 
+h = 2e-3
+
+def sigma_theta(theta):
+    return - rho * g * np.cos(theta) / (np.sin(theta))**2
+
+def sigma_phi(theta):
+    return - rho * g * r * np.cos(theta) / (np.sin(theta))**2 * (1 + (np.sin(theta))**2)
+
+theta_vals = np.linspace(np.pi/4, np.pi/2, 100)
+
+sigma_theta_vals = sigma_theta(theta_vals)
+sigma_phi_vals = sigma_phi(theta_vals)
+
+
+plt.figure()
+plt.plot(theta_vals, sigma_theta_vals, label='sigma_theta')
+plt.plot(theta_vals, sigma_phi_vals,'--',label='sigma_phi')
+plt.legend()
+plt.grid()
+plt.show()
+
+def sigma_vm(theta):
+    sigma_theta = - rho * g * r * np.cos(theta) / np.sin(theta)**2
+    sigma_phi = - rho * g * r * np.cos(theta) / np.sin(theta)**2 * (1 + np.sin(theta)**2)
+    return 1/np.sqrt(2) * np.sqrt((sigma_theta - sigma_phi)**2 + sigma_theta**2 + sigma_phi**2)
+
+sigma_vm_vals = sigma_vm(theta_vals)
+
+plt.figure()
+plt.plot(theta_vals, sigma_vm_vals, label='sigma_vm')
+plt.legend()
+plt.grid()
+plt.show()
---
+++ TME235/old exams/axisymmetry.py
@@ -0,0 +1,55 @@
+#%%
+# Computer assignment 3 träning
+import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
+
+r, nu, E, h, q, a, b = sp.symbols('r nu E h q a b', real=True)
+
+D = E * h**3 / (12 * (1 - nu**2))
+
+w = sp.Function('w')(r)
+
+diffeq = sp.Eq(1/r * sp.diff(r * sp.diff(1 / r * sp.diff(r * sp.diff(w, r), r), r), r), q / D)
+
+w = sp.dsolve(diffeq, w)
+w = w.rhs
+print('\nw:')
+print(w)
+
+# Formula sheet:
+M_r = D * (- w.diff(r, 2) - nu * w.diff(r) / r)
+M_phi = D * (- nu * w.diff(r, 2) - w.diff(r) / r)
+
+V = r * sp.diff(r * M_r, r) - M_phi
+
+boundary_conditions = [
+    w.subs(r, b),
+    M_r.subs(r, b),
+    M_r.subs(r, a),
+    V.subs(r, a)
+]
+unknowns = ('C1, C2, C3, C4')
+integration_constants = sp.solve(boundary_conditions, unknowns, real=True)
+print('\nintegration constants:')
+print(integration_constants)
+
+w_sol = w.subs(integration_constants)
+print('w_sol:')
+print(w_sol)
+w_f = sp.lambdify((r, nu, E, h, q, a, b), w_sol, 'numpy')
+
+r_vals = np.linspace(0.1, 0.3, 500)
+
+nu_num = 0.3
+E_num = 210e9
+q_num = 15e6
+a_num = 0.1
+b_num = 0.3
+h_num = a_num/4
+
+w_vals = w_f(r=r_vals, nu=nu_num, E=E_num, h=h_num, q=q_num, a=a_num, b=b_num)
+
+plt.figure()
+plt.plot(r_vals, w_vals)
+plt.show()
