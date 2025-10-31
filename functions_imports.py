2855497732cc50f8c8976ecedce7484565cef5be
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-10-31 17:41:31 +0100

    .

--- functions_imports.py
+++ functions_imports.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from sympy import *
+import sympy as sp
 import math
 import pandas as pd
 
@@ -14,7 +14,7 @@ from IPython.display import HTML
 from matplotlib import animation
 import scipy.io as sio
 from scipy.optimize import fsolve
-from matplotlib import rcParams # for changing default values
+from matplotlib import rcParams
 import matplotlib.ticker as ticker
 
 import calfem.core as cfc
@@ -28,7 +28,7 @@ import matplotlib.cm as cm
 from pathlib import Path
 
 def new_prob(string):
-    print_string = '\n--------------------------------------------\n' + 'Assignment ' + str(string) + '\n--------------------------------------------\n'
+    print_string = '\n' + '=' * 80 + '\n' + 'Assignment ' + str(string) + '\n' + '=' * 80 + '\n'
     return print(print_string)
 
 SMALL_SIZE = 10
@@ -36,14 +36,14 @@ MEDIUM_SIZE = 12
 BIGGER_SIZE = 14
 dpi = 500
 
-# Set the global font sizes
-plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.rc('font', size=SMALL_SIZE)          
+plt.rc('axes', titlesize=BIGGER_SIZE)     
+plt.rc('axes', labelsize=MEDIUM_SIZE)    
+plt.rc('xtick', labelsize=SMALL_SIZE)    
+plt.rc('ytick', labelsize=SMALL_SIZE)    
+plt.rc('legend', fontsize=SMALL_SIZE)    
+plt.rc('figure', titlesize=BIGGER_SIZE)
+
 plt.rc('figure', figsize=(8,4))
 
 script_dir = Path(__file__).parent
