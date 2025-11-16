91cbc6a6f82a33aefdc4b8323ddab0e27de6665a
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-11-16 16:56:20 +0100

    .

--- MTF073/T1/Martin_codeFunctions.py
+++ MTF073/T1/Martin_codeFunctions.py
@@ -371,9 +371,12 @@ def calcCoeffs(aE, aW, aN, aS, aP,
             aN[i,j] = k_n[i,j] * A_NS[i,j] / dy_PN[i,j]
             aS[i,j] = k_s[i,j] * A_NS[i,j] / dy_SP[i,j]
             
-            if j == nJ - 1:
+            if j == nJ - 2:
                 aN[i,j] = 0
+                
+            if j == 1:
                 aS[i,j] = 0
+                
                 
     # Modifications of aE and aW inside east and west boundaries:
     # ADD CODE HERE IF NECESSARY
