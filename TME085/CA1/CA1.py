77728a532a628f5c937c707d9d08d82db168c7a8
MartinStjernkvist <mr.martin.pro@gmail.com>
2026-02-06 15:00:24 +0100

    .

--- TME085/CA1/CA1.py
+++ TME085/CA1/CA1.py
@@ -90,7 +90,9 @@ def task_1a(T1, M1, p1, rho1, Cp):
     # From Table A.3:
     # 7.00000e-01 1.42349e+00 9.92895e-01 1.43367e+00 1.04310e+00 9.08499e-01
     # 7.20000e-01 1.39069e+00 1.00260e+00 1.38709e+00 1.03764e+00 9.22122e-01
-    M2 = interpolation(9.08499e-01, 7.00000e-01, 9.22122e-01, 7.20000e-01, T02_T0_star)
+    M2 = interpolation(9.08499e-01, 7.00000e-01, 
+                       9.22122e-01, 7.20000e-01, 
+                       T02_T0_star)
     print(f'M2: {M2:.2f}')
     
     T2 = T2_func(T1, M1, M2)
@@ -135,7 +137,9 @@ def task_1b(T1, M1, p1, gamma, R):
     # From Table A.3:
     # 1.18000e+00 8.13736e-01 9.22000e-01 8.82577e-01 1.01573e+00 9.82299e-01
     # 1.20000e+00 7.95756e-01 9.11848e-01 8.72685e-01 1.01942e+00 9.78717e-01
-    M2 = interpolation(9.82299e-01, 1.18000e+00, 9.78717e-01, 1.20000e+00, T02_T0_star)
+    M2 = interpolation(9.82299e-01, 1.18000e+00, 
+                       9.78717e-01, 1.20000e+00, 
+                       T02_T0_star)
     print(f'M2: {M2:.2f}')
     
     T2 = T2_func(T1, M1, M2)
@@ -169,12 +173,16 @@ def task_2a(T1, M1, p1, D, fbar, gamma, R):
     L = 0.8 * Lstar1
     print(f'L: {L:.2f}')
     Lstar2 = Lstar1 - L
+    print(f'Lstar2: {Lstar2:.2f}')
+    
     frac_4fLstar2_D = 4 * fbar * Lstar2 / D
     print(f'fraction: {frac_4fLstar2_D:.4f}')
     # From Table A.4:
     # 3.60000e-01 1.16968e+00 3.00422e+00 2.56841e+00 1.73578e+00 3.18012e+00
     # 3.80000e-01 1.16632e+00 2.84200e+00 2.43673e+00 1.65870e+00 2.70545e+00
-    M2 = interpolation(3.18012e+00,3.60000e-01, 2.70545e+00, 3.80000e-01, frac_4fLstar2_D)
+    M2 = interpolation(3.18012e+00, 3.60000e-01, 
+                       2.70545e+00, 3.80000e-01, 
+                       frac_4fLstar2_D)
     print(f'M2: {M2:.2f}')
     
     T2 = T2_fric_func(T1, M1, M2)
@@ -186,6 +194,7 @@ def task_2a(T1, M1, p1, D, fbar, gamma, R):
     
 task_2a(T1_2a, M1_2a, p1_2a, D_2a, fbar_2a, gamma_2a, R_2a)
 
+#%%
 # Task 2.b
 printt('Task 2.b:')
 D_2b = 0.3
@@ -201,19 +210,23 @@ def task_2b(T1, M1, p1, D, fbar, gamma, R):
     rho1 = rho_func(p1, R, T1)
     print(f'rho1_1b: {rho1:.2f}')    
     
-    frac_4fLstar1_D = 1.45333e+01 # from A.4, M1 = 2.0
+    frac_4fLstar1_D = 3.04997e-01 # from A.4, M1 = 2.0
     Lstar1 = frac_4fLstar1_D * D / (4 * fbar)
     print(f'Lstar1: {Lstar1:.2f}')
     
-    L = 1.5 * Lstar1
+    L = 0.8 * Lstar1
     print(f'L: {L:.2f}')
     Lstar2 = Lstar1 - L
+    print(f'Lstar2: {Lstar2:.2f}')
+    
     frac_4fLstar2_D = 4 * fbar * Lstar2 / D
     print(f'fraction: {frac_4fLstar2_D:.4f}')
     # From Table A.4:
-    # 3.60000e-01 1.16968e+00 3.00422e+00 2.56841e+00 1.73578e+00 3.18012e+00
-    # 3.80000e-01 1.16632e+00 2.84200e+00 2.43673e+00 1.65870e+00 2.70545e+00
-    M2 = interpolation(3.18012e+00,3.60000e-01, 2.70545e+00, 3.80000e-01, frac_4fLstar2_D)
+    # 1.28000e+00 9.03832e-01 7.42735e-01 8.21762e-01 1.05810e+00 5.82014e-02
+    # 1.30000e+00 8.96861e-01 7.28483e-01 8.12258e-01 1.06630e+00 6.48321e-02
+    M2 = interpolation(5.82014e-02, 1.28000e+00, 
+                       6.48321e-02, 1.30000e+00, 
+                       frac_4fLstar2_D)
     print(f'M2: {M2:.2f}')
     
     T2 = T2_fric_func(T1, M1, M2)
@@ -223,8 +236,31 @@ def task_2b(T1, M1, p1, D, fbar, gamma, R):
     print(f'p2: {p2:.2f}')
     print(f'rho2: {rho2:.2f}')
     
-task_2b(T1_2b, M1_2b, p1_2b, D_2b, fbar_2a, gamma_2a, R_2a)
+task_2b(T1_2b, M1_2b, p1_2b, D_2b, fbar_2b, gamma_2b, R_2b)
 
+#%%
+# Task 2.c
+printt('Task 2.c:')
+D_2c = 0.3
+fbar_2c = 0.005
+M1_2c = 2.0
+T1_2c = 293
+p1_2c = 1 * bar_to_Pa
+gamma_2c = gamma_air
+R_2c = R_univ
 
+def task_2c(T1, M1, p1, D, fbar, gamma, R):
+    
+    rho1 = rho_func(p1, R, T1)
+    print(f'rho1_1b: {rho1:.2f}')    
+    
+    frac_4fLstar1_D = 3.04997e-01 # from A.4, M1 = 2.0
+    Lstar1 = frac_4fLstar1_D * D / (4 * fbar)
+    print(f'Lstar1: {Lstar1:.2f}')
+    
+    L = 1.5 * Lstar1
+    print(f'L: {L:.2f}')
+    
+task_2c(T1_2c, M1_2c, p1_2c, D_2c, fbar_2c, gamma_2c, R_2c)
 
 #%%
